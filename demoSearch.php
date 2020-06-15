326da18a74bd8dba719722053110d30aa3e4f229
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-06-15 11:30:49 +0800

    Update demoSearch.php

--- demoSearch.php
+++ demoSearch.php
@@ -1,6 +1,16 @@
 <html>
+<head>
+	<link rel="stylesheet" href="http://code.jquery.com/ui/1.12.1/themes/cupertino/jquery-ui.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/free-jqgrid/4.13.5/css/ui.jqgrid.min.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+  	<script src="https://cdnjs.cloudflare.com/ajax/libs/free-jqgrid/4.13.5/js/jquery.jqgrid.min.js"></script>
+</head>
 <body>
-<div class="container" style="padding:50px 250px;">
+ 	<table id="photos" class="display" cellspacing="0" width="100%">
+	</table>
+	<div id="pager2"></div>
+
+ <div class="container" style="padding:50px 250px;">
 	<form role="form">
         <div class="form-group">
           <input type="input" class="form-control input-lg" id="txt-search" placeholder="Type your search character">
@@ -45,5 +55,58 @@ $('#txt-search').keyup(function(){
         });
    });
  </script>
+
+<script type="text/javascript">
+  $(document).ready(function(){
+		
+	$("#photos").jqGrid({
+		url:'https://iberkat.tech/afms/afmsAPI/api/employeeData/read.php',
+		datatype: "json",
+		colNames:['ID', 'No IC', 'Sex', 'Station Code'],
+		colModel:[
+			{name:'id',index:'id', width:55},
+			{name:'noIC',index:'noIC', width:75},
+			{name:'sex',index:'sex', width:55},	
+			{name:'statonCode',index:'statonCode', width:55}		
+		],
+		rowNum:10,
+		loadonce: true,
+		rowList:[10,20,30],
+		pager: '#pager2',
+		sortname: 'id',
+		viewrecords: true,
+		sortorder: "desc",
+		caption:""
+	});
+	$("#photos").jqGrid('navGrid','#pager2',{edit:false,add:false,del:false});
+    /*var arrayReturn = [];
+            $.ajax({
+                url: "https://jsonplaceholder.typicode.com/posts",
+                async: false,
+                dataType: 'json',
+                success: function (data) {
+					for (var i = 0, len = data.length; i < len; i++) {
+						var desc = data[i].body;
+						arrayReturn.push([ data[i].userId, '<a href="http://google.com" target="_blank">'+data[i].title+'</a>', desc.substring(0, 12)]);
+					}
+				inittable(arrayReturn);
+                }
+            });
+	function inittable(data) {	
+		//console.log(data);
+		$('#photos').DataTable( {
+			"aaData": data,
+			"dataSrc": function ( json ) {
+				console.log(json);
+			  for ( var i=0, ien=json.data.length ; i<ien ; i++ ) {
+				json.data[i][0] = '<a href="/message/'+json.data[i][0]+'>View message</a>';
+			  }
+			  return json.data;
+			}
+		} );
+	}*/
+  });
+</script>
+
  </body>
  </html>
