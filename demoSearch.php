69905e10700c3441955e5156e72f6d36b55d0f39
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-06-15 14:42:03 +0800

    Update demoSearch.php

--- demoSearch.php
+++ demoSearch.php
@@ -59,8 +59,8 @@
 $(document).ready(function(){
 
 var staticUrl = 'https://iberkat.tech/afms/afmsAPI/api/employeeData/read.php';
-$.getJSON(staticUrl, function(data) {
-			  console.log(data); });
+var data = $.getJSON(staticUrl, function(data2) {
+			  console.log(data2); });
 
 $('#txt-search').keyup(function(){
             var searchField = $(this).val();
