77054ff4ed537b98d0a9f4bcf07f610d11afe292
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-06-15 11:56:54 +0800

    Update demoSearch.php

--- demoSearch.php
+++ demoSearch.php
@@ -31,11 +31,16 @@
 <link rel="preload" href="https://adservice.google.com.my/adsid/integrator.js?domain=js-tutorials.com" as="script"><script type="text/javascript" src="https://adservice.google.com.my/adsid/integrator.js?domain=js-tutorials.com"></script><link rel="preload" href="https://adservice.google.com/adsid/integrator.js?domain=js-tutorials.com" as="script"><script type="text/javascript" src="https://adservice.google.com/adsid/integrator.js?domain=js-tutorials.com"></script>
 </head>
 <body>
-	<div class="col col-md-12">
+	
+<section class="content">
+<div class="content-wrapper">
+	<div class="col-sm-12">
 	 	<table id="photos" class="display table-responsive" cellspacing="0" width="100%">
 		</table>
 		<div id="pager2"></div>
     </div>
+</div>
+</section>
 
  <div class="container" style="padding:50px 250px;">
 	<form role="form">
